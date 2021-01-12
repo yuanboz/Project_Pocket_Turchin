5cb14692d1d24269d5480159e6ba77bc503531bb
yuanboz <yuanboz@appstate.edu>
2021-01-11 21:09:25 -0500

    working on contentVC

--- Project_PocketTurchin/ViewController/Gallery/DetailPageViewController.swift
+++ Project_PocketTurchin/ViewController/Gallery/DetailPageViewController.swift
@@ -13,6 +13,8 @@ class DetailPageViewController: UIViewController, FloatingPanelControllerDelegat
     @IBOutlet var coverImageView: UIImageView!
     
     var coverImage: UIImage?
+    var exhibitionTitle: String?
+    var exhibitionDate: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,13 +40,5 @@ class myPanelLandscapeLayout: FloatingPanelLayout {
 //            .tip: FloatingPanelLayoutAnchor(absoluteInset: 44.0, edge: .bottom, referenceGuide: .safeArea),
         ]
     }
-    
-    func floatingPanelDidMove(_ vc: FloatingPanelController) {
-        if vc.isAttracting == false {
-            let loc = vc.surfaceLocation
-            let minY = vc.surfaceLocation(for: .full).y - 6.0
-            let maxY = vc.surfaceLocation(for: .half).y + 6.0
-            vc.surfaceLocation = CGPoint(x: loc.x, y: min(max(loc.y, minY), maxY))
-        }
-    }
+
 }
