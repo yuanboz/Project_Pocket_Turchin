5a8d9fcdab99175a1e39ebbfb30a31a7d6235791
yuanboz <yuanboz@appstate.edu>
2021-02-07 15:49:33 -0500

    add a collection view to display multiple images in detail page

---
+++ Project_PocketTurchin/ViewController/Detail page/ContentViewCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ContentViewCollectionViewCell.swift
+//  Project_PocketTurchin
+//
+//  Created by 周元博 on 2/7/21.
+//
+
+import UIKit
+
+class ContentViewCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var contentImageView: UIImageView!
+}
--- Project_PocketTurchin/ViewController/Detail page/ContentViewController.swift
+++ Project_PocketTurchin/ViewController/Detail page/ContentViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ContentViewController: UIViewController {
+class ContentViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
     
     @IBOutlet var exhibitionTitleLabel: UILabel!
     
@@ -17,15 +17,34 @@ class ContentViewController: UIViewController {
     
     @IBOutlet var likedButton: UIButton!
     
+    @IBOutlet var exhibitionDescriptionTextView: UITextView!
+    
+    @IBOutlet var slideCollectionView: UICollectionView!
+    
     var exhibitionTitle: String?
     
     var exhibitionDate: String?
     
     var liked: Bool = false
+    
+    let cellScale: CGFloat = 0.6
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        slideCollectionView.delegate = self
+        slideCollectionView.dataSource = self
         setUpInfo()
+
+        let screenSize = UIScreen.main.bounds.size
+        let cellWidth = floor(screenSize.width * cellScale)
+        let cellHeight = floor(screenSize.height * cellScale)
+        let insertX = (view.bounds.width - cellWidth) / 2.0
+        let insertY = (view.bounds.height - cellHeight) / 2.0
+        
+        let layout = slideCollectionView!.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        slideCollectionView.contentInset = UIEdgeInsets(top: insertY, left: insertX, bottom: insertY, right: insertX)
+        
     }
     
     func setUpInfo() {
@@ -48,5 +67,36 @@ class ContentViewController: UIViewController {
     @IBAction func likedButtonTapped(_ sender: UIButton) {
         likedButtonAnimation()
     }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = slideCollectionView.dequeueReusableCell(withReuseIdentifier: "contentviewcell", for: indexPath) as! ContentViewCollectionViewCell
+        
+        return cell
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let layout = self.slideCollectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+        
+        var offset = targetContentOffset.pointee
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+        let roundedIndex = round(index)
+        
+        offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: scrollView.contentInset.top)
+        
+        targetContentOffset.pointee = offset
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: slideCollectionView.frame.width, height: slideCollectionView.frame.height)
+//    }
 
 }
